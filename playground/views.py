ec64fce5a883281413a509abb35f4db9880d36a4
sanj94 <sanjar.haydarov1994@mail.ru>
2022-09-19 23:27:08 +0200

    too many files added

---
+++ playground/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .tasks import notify_customers
+
+
+def say_hello(request):
+    notify_customers.delay('Hello')
+    return render(request, 'hello.html', {'name': 'Sanjar'})
+    
---
+++ store/tests/test_collections.py
@@ -0,0 +1,26 @@
+import pytest
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+@pytest.mark.django_db
+class TestCreateCollection:
+    def test_if_user_is_anonymous_returns_401(self):
+        client = APIClient()
+        response = client.post('/store/collections/', {'title': 'a'})
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', {'title': 'a'})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_user_is_not_admin_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', {'title': 'a'})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
